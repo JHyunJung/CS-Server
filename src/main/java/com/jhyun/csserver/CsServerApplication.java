2fc30e600be89e39f83e08b576647c5f7835c580
JhyunJung <jhyun.rigel@gmail.com>
2023-01-05 20:39:25 +0900

    init Project

---
+++ src/main/java/com/jhyun/csserver/CsServerApplication.java
@@ -0,0 +1,13 @@
+package com.jhyun.csserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhyun/csserver/CsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhyun.csserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
